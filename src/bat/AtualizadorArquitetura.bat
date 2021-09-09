90bb4ba106552640a14541c9724bcee2e7ee8719
Paulo Roberto <administrador.paulo@gmail.com>
2021-09-09 17:17:07 -0300

    ajuste no atualizador de arquitetura

--- src/bat/AtualizadorArquitetura.bat
+++ src/bat/AtualizadorArquitetura.bat
@@ -8,13 +8,12 @@ set namespace=%namespace:~0,-5%
 set namespace=%namespace:"=%
 set nameContext=%namespace%Context
 
-echo Gerando Arquivos para Entidade %entidade% Soluction %namespace% DBContext %nameContext%
-
+echo Gerando Arquivos para Entidade '%entidade%' Solution '%namespace%' DBContext '%nameContext%'
 
 set diretorioSRC=%cd%
 
 echo Criando Service Base
-CALL :ServiceBase %namespace% %nameContext% > %diretorioSRC%\Domain\%namespace%.Domain.Services\Services\ServiceBase.cs  
+CALL :ServiceBase %namespace% %nameContext% > %diretorioSRC%\Domain\%namespace%.Domain.Services\Services\ServiceBase.cs
 echo Criando Repositorie Base
 CALL :RepositorieBase %namespace% %nameContext% > %diretorioSRC%\Infra\%namespace%.Infra.Data\Repositories\RepositorieBase.cs 
 echo Criando Contrato de Repositorio Base
@@ -48,14 +47,16 @@ echo 'Processo Concluido!'
 exit /b
 
 :IServiceBase
-
+echo using System.Collections.Generic;
+echo.
 echo namespace %1.Domain.Contracts.Services
 echo {
 echo     public interface IServiceBase^<TEntity^> where TEntity : class
 echo     {
-echo         TEntity Incluir(TEntity entity);
-echo         TEntity Alterar(TEntity entity);
-echo         void Excluir(TEntity entity);
+echo         TEntity Add(TEntity entity);
+echo         TEntity Update(TEntity entity);
+echo         void Delete(TEntity entity);
+echo         List^<string^> GetAllAnnotations();
 echo     }
 echo }
 
@@ -70,11 +71,11 @@ echo namespace %1.Domain.Contracts.Repositories
 echo {
 echo     public interface IRepositorieBase^<TEntity^> where TEntity : class
 echo     {
-echo         TEntity Incluir(TEntity entity);
-echo         TEntity Alterar(TEntity entity);
-echo         void Excluir(TEntity entity);
-echo         TEntity Obter(Guid id);
-echo         List^<TEntity^> Obter();
+echo         TEntity Add(TEntity entity);
+echo         TEntity Update(TEntity entity);
+echo         void Delete(TEntity entity);
+echo         TEntity Get(Guid id);
+echo         List^<TEntity^> Get();
 echo     }
 echo }
 
@@ -99,29 +100,29 @@ echo         {
 echo             this.context = context;
 echo         }
 echo.
-echo         public virtual TEntity Alterar(TEntity entity)
+echo         public virtual TEntity Update(TEntity entity)
 echo         {
 echo             context.Set^<TEntity^>().Update(entity);
 echo             return entity;
 echo         }
 echo.
-echo         public virtual void Excluir(TEntity entity)
+echo         public virtual void Delete(TEntity entity)
 echo         {
 echo             context.Set^<TEntity^>().Remove(entity);
 echo         }
 echo.
-echo         public virtual TEntity Incluir(TEntity entity)
+echo         public virtual TEntity Add(TEntity entity)
 echo         {
 echo             context.Set^<TEntity^>().Add(entity);            
 echo             return entity;
 echo         }
 echo.
-echo         public virtual TEntity Obter(Guid id)
+echo         public virtual TEntity Get(Guid id)
 echo         {
 echo             return context.Set^<TEntity^>().Find(id);
 echo         }
 echo.
-echo         public virtual List^<TEntity^> Obter()
+echo         public virtual List^<TEntity^> Get()
 echo         {
 echo             return context.Set^<TEntity^>().ToList();
 echo         }
@@ -148,6 +149,7 @@ exit /b
 echo using %1.Domain.Contracts.Repositories;
 echo using %1.Domain.Contracts.Services;
 echo using %1.Infra.Data.EF;
+echo using System.Collections.Generic;
 echo.
 echo namespace %1.Domain.Services
 echo {
@@ -155,28 +157,31 @@ echo     public class ServiceBase^<TEntity^> : IServiceBase^<TEntity^> where TEn
 echo     {
 echo         private readonly IRepositorieBase^<TEntity^> repositorieBase;
 echo         private readonly %2 context;
+echo         private readonly List^<string^> Annotations;
+echo.
 echo         public ServiceBase(IRepositorieBase^<TEntity^> repositorieBase, %2 context)
 echo         {
 echo             this.context = context;
 echo             this.repositorieBase = repositorieBase;
+echo             Annotations = new List^<string^>();
 echo         }
 echo.
-echo         public virtual TEntity Alterar(TEntity entity)
+echo         public virtual TEntity Update(TEntity entity)
 echo         {
-echo             entity = repositorieBase.Alterar(entity);
+echo             entity = repositorieBase.Update(entity);
 echo             Commit();
 echo             return entity;
 echo         }
 echo.
-echo         public virtual void Excluir(TEntity entity)
+echo         public virtual void Delete(TEntity entity)
 echo         {
-echo             repositorieBase.Excluir(entity);
+echo             repositorieBase.Delete(entity);
 echo             Commit();
 echo         }
 echo.
-echo         public virtual TEntity Incluir(TEntity entity)
+echo         public virtual TEntity Add(TEntity entity)
 echo         {
-echo             entity = repositorieBase.Incluir(entity);
+echo             entity = repositorieBase.Add(entity);
 echo             Commit();
 echo             return entity;
 echo         }
@@ -184,6 +189,16 @@ echo.
 echo         protected void Commit()
 echo         {
 echo             context.SaveChanges();
+echo         }
+echo.
+echo         protected void IncludeAnnotation(string message)
+echo         {
+echo             Annotations.Add(message);
+echo         }
+echo.
+echo         public List^<string^> GetAllAnnotations()
+echo         {
+echo            return Annotations;
 echo         }
 echo.
 echo     }
@@ -235,7 +250,7 @@ echo.
 echo         // GET: %1s
 echo         public IActionResult Index()
 echo         {
-echo             return View(Repositorie%1.Obter());
+echo             return View(Repositorie%1.Get());
 echo         }
 echo. 
 echo         [HttpPost]
@@ -247,7 +262,7 @@ echo                 ViewBag.Erro = "Selecione os tipos de pesquisa e informe os
 echo                 return View(new List^<%1^>());
 echo             }
 echo.            
-echo             return View(Repositorie%1.Obter());
+echo             return View(Repositorie%1.Get());
 echo         }
 echo. 
 echo         // GET: %1s/Details/5
@@ -258,7 +273,7 @@ echo             {
 echo                 return NotFound();
 echo             }
 echo. 
-echo             %1 %1 = Repositorie%1.Obter((Guid)id);
+echo             %1 %1 = Repositorie%1.Get((Guid)id);
 echo             if (%1 == null)
 echo             {
 echo                 return NotFound();
@@ -282,7 +297,7 @@ echo         public IActionResult Create(%1 %1)
 echo         {
 echo             if (ModelState.IsValid)
 echo             {
-echo                 Service%1.Incluir(%1);
+echo                 Service%1.Add(%1);
 echo                 return RedirectToAction(nameof(Index));
 echo             }
 echo             return View(%1);
@@ -296,7 +311,7 @@ echo             {
 echo                 return NotFound();
 echo             }
 echo. 
-echo             %1 %1 = Repositorie%1.Obter((Guid)id);
+echo             %1 %1 = Repositorie%1.Get((Guid)id);
 echo. 
 echo             if (%1 == null)
 echo             {
@@ -321,7 +336,7 @@ echo             if (ModelState.IsValid)
 echo             {
 echo                 try
 echo                 {
-echo                     Service%1.Alterar(%1);
+echo                     Service%1.Update(%1);
 echo                 }
 echo                 catch (DbUpdateConcurrencyException)
 echo                 {
@@ -347,7 +362,7 @@ echo             {
 echo                 return NotFound();
 echo             }
 echo. 
-echo             %1 %1 = Repositorie%1.Obter((Guid)id);
+echo             %1 %1 = Repositorie%1.Get((Guid)id);
 echo             if (%1 == null)
 echo             {
 echo                 return NotFound();
@@ -361,18 +376,18 @@ echo         [HttpPost, ActionName("Delete")]
 echo         [ValidateAntiForgeryToken]
 echo         public IActionResult DeleteConfirmed(Guid id)
 echo         {
-echo             var %1 = Repositorie%1.Obter(id);
+echo             var %1 = Repositorie%1.Get(id);
 echo             if (%1 == null)
 echo             {
 echo                 return NotFound();
 echo             }
-echo             Service%1.Excluir(%1);
+echo             Service%1.Delete(%1);
 echo             return RedirectToAction(nameof(Index));
 echo         }
 echo. 
 echo         private bool %1Exists(Guid id)
 echo         {
-echo             return Repositorie%1.Obter(id) != null;
+echo             return Repositorie%1.Get(id) != null;
 echo         }
 echo     }
 echo }
@@ -397,7 +412,7 @@ echo         private readonly IRepositorie%1 repositorie%1;
 echo.
 echo         public Service%1(IRepositorie%1 repo, %3 context) : base(repo, context)
 echo         {
-echo.
+echo             repositorie%1 = repo;
 echo         }
 echo     }
 echo }
